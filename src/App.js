602c37742d94803cc9a5ce2e1c40cdba8781bad8
torben <torben@accountable.eu>
2022-07-16 10:43:42 +0100

    feat/rateCalculator: add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Form from "./components/Form";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-slate-50">
+      <div className="border-slate-400 border-2 bg-slate-400 p-5 flex flex-col gap-4">
+        <Header />
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default App;
