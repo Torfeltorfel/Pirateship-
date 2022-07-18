62590ee4ffba08b98d65880ed118028e8d24b864
torben <torben@accountable.eu>
2022-07-18 18:21:19 +0100

    feat/rateCalculator: rename Form component to rateCalculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Form from "./components/Form";
+import RateCalculator from "./components/RateCalculator";
 
 function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-slate-50">
       <div className="border-slate-400 border-2 bg-slate-400 p-5 flex flex-col gap-4">
-        <Form />
+        <RateCalculator />
       </div>
     </div>
   );
